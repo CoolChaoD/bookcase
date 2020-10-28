f00409e9d705118e5a2b1795f48aff79102a4819
dongchao <3291240985@qq.com>
2020-10-28 20:41:01 +0800

    完成了递减运算符的重载，记得左移运算符重载有返回值

--- C++语言程序设计/04递减运算符的重载.cpp
+++ C++语言程序设计/04递减运算符的重载.cpp
@@ -1,3 +1,49 @@
 /*
   递减运算符的实现
 */
+#include <iostream>
+
+using namespace std;
+
+class MyIntenger {
+
+	friend ostream& operator<<(ostream& out, MyIntenger myint);
+public:
+	MyIntenger() {
+		m_Num = 0;
+	}
+	//前置递减返回值是引用
+	MyIntenger& operator--() {
+		this->m_Num--;
+		return *this;
+	}
+
+	//后置递减返回值是数值
+	MyIntenger operator--(int) {
+		MyIntenger temp = *this;
+		this->m_Num--;
+		return temp;
+	}
+private:
+	int m_Num;
+};
+
+ostream& operator<<(ostream& out, MyIntenger myint) {
+	out << "m_Num=" << myint.m_Num << endl;
+	return out;
+}
+
+void test01() {
+	MyIntenger m;
+	cout << m-- << endl;
+	cout << m << endl;
+
+}
+
+
+int main() {
+	test01();
+	system("pause");
+	return 1;
+
+}
