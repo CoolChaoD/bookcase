513badf2186499d66eaa967665cd4610d5c9562f
dongchao <3291240985@qq.com>
2020-11-03 22:55:10 +0800

    编写函数模板

---
+++ C++语言程序设计/01函数模板.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+//函数模板
+//交换两个数
+template <class T>
+void mySwap(T& a, T& b) {
+
+	T temp = a;
+	a = b;
+	b = temp;
+
+}
+
+void test01() {
+	int a = 10;
+	int b = 20;
+
+	//1.指定显示类型
+	mySwap<int>(a, b);
+	cout << "a=" << a << endl;
+	cout << "b=" << b << endl;
+
+	//2.自动类型推导
+	double c = 1.1;
+	double d = 2.2;
+	mySwap(c, d);
+	cout << "c=" << c << endl;
+	cout << "d=" << d << endl;
+
+
+}
+
+int main() {
+
+	test01();
+	system("pause");
+	return 1;
+}
