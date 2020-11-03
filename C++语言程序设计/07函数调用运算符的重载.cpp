f2393c70fbd640db13a90565141fdb53fb9be76c
dongchao <3291240985@qq.com>
2020-11-03 08:31:52 +0800

    完成了仿函数的代码的编写

---
+++ C++语言程序设计/07函数调用运算符的重载.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Myprint {
+public:
+	//重载函数调用运算符
+	void operator()(string test) {
+		cout << test << endl;
+	}
+};
+
+//仿函数的写法十分灵活，没有固定的写法
+class Myadd {
+public:
+	int operator()(int num1, int num2) {
+		return num1 + num2;
+	}
+
+
+};
+
+
+void test01() {
+	Myprint myprint;
+	myprint("dongchao");
+}
+
+void test02() {
+	Myadd myadd;
+
+	int res = myadd(100, 12);
+	cout << res << endl;
+	//匿名函数对象
+	cout << "匿名函数对象" << Myadd()(100, 122) << endl;
+
+}
+
+int main() {
+
+	test01();
+	test02();
+	return 1;
+}
+
+
